518601ee1f64fb599b7df1f46a172934b9464791
Angel Garcia <angel.garc.ort@gmail.com>
2024-07-07 18:25:18 -0600

    first commit

---
+++ app/todo/actions/todo.action.ts
@@ -0,0 +1,47 @@
+"use server";
+import { prisma } from "@/libs/prismadb";
+import { revalidatePath } from "next/cache";
+
+export const createTodo = async (title: string) => {
+  if (!title || !title.trim()) {
+    return { error: "Title is required" };
+  }
+
+  try {
+    await prisma.todo.create({
+      data: {
+        title,
+      },
+    });
+    revalidatePath("/todo");
+    return {success: true}
+  } catch (error) {
+    return{
+        error:"Error creating todo (Backend)"
+    }
+  }
+};
+
+export const removeTodo = async(id:string) =>{
+  if(!id || !id.trim()){
+    return {
+      error:"Id is required (Backend)"
+    }
+  }
+
+  try {
+    await prisma.todo.delete({
+      where:{
+        id,
+      }
+    })
+    revalidatePath("/todo")
+    return{
+      success: true,
+    }
+  } catch (error) {
+    return{
+      error: "Error removing todo (backend)"
+    }
+  }
+}
